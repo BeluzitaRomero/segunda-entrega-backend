774b57037168cd0809bbfd824bdc9fa6302566b2
Belen Romero <romero.abelen@gmail.com>
2022-05-13 22:24:24 -0300

    eliminados

--- src/daos/index.js
+++
@@ -1,8 +0,0 @@
-// //Hacer un switch para usar la db que se requira
-// let daoProduct;
-// let daoCart;
-
-// //Funcion con el switch
-
-// export { daoProduct, daoCart };
-// //las vamos a usar en nuestro index o carpeta de rutas
