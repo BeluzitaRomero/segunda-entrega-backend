956cf76fe6750d308135200fd6780a8da4cbfdbf
Belen Romero <romero.abelen@gmail.com>
2022-06-04 21:53:48 -0300

    desafio15

--- src/main.js
+++ src/main.js
@@ -3,10 +3,25 @@ const args = require("../yargs/yargs");
 
 const PORT = args.port; //Puerto desde yargs
 
-const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
-});
+const numCPUs = require("os").cpus().length;
+const cluster = require("cluster");
 
-server.on("error", (error) => {
-  console.log(`Error en el servidor ${error}`);
-});
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    //worker (uno para cada nucleo)
+    cluster.fork(); // creacion de subproceso
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Process ${process.pid} died`);
+  });
+} else {
+  const server = app.listen(PORT, () => {
+    console.log(
+      `Servidor http escuchando en el puerto ${server.address().port}`
+    );
+  });
+
+  server.on("error", (error) => {
+    console.log(`Error en el servidor ${error}`);
+  });
+}
